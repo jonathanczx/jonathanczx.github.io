dc614531cd6ebe6cc6f634c918be1d2972d19d79
Jonathan Chan <jonathanczx@gmail.com>
2017-07-12 01:56:13 +1000

    Added GitHub and LinkedIn icon links to my profiles and updated information

--- js/cvScripts.js
+++ js/cvScripts.js
@@ -1,3 +1,21 @@
+//Run after document is ready
 $(document).ready(function(){
-	$("main").fadeIn(1000).removeClass("hidden");
+  $("main.hidden").fadeIn(1000).removeClass("hidden");
+  
+  //OnClick Event Handler for Rows
+  $(".row").on("click", function() {
+	  console.log("Row Clicked");
+	  if($(this).hasClass("expanded"))
+	  {
+	  	$(this).removeClass("expanded");
+	  	$(this).css("background-color", "#FFF");
+	  }
+	  else 
+	  {
+	  	$(this).addClass("expanded");
+	  	$(this).css("background-color", "#E0E0E0");
+	  }
+	  $(this).next(".expansion").slideToggle();
+  });
 });
+
